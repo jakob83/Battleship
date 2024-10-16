f404fcb04b2b44046477ec729db8f21259563522
jakob83 <jakobpllo@gmail.com>
2024-10-16 16:07:48 +0200

    Setup npm, webpack, jest, eslint

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// eslint-disable-next-line no-undef
+const path = require('path');
+// eslint-disable-next-line no-undef
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  output: {
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // Output filename
+  },
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/template.html',
+      title: 'Webpack Asset Management',
+      inject: 'body',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/', // output path in your build folder
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+};
